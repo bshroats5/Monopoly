dd2e321ee92fb4a91c99e892433672ae709112de
Bret Shroats <bshroats5@gmail.com>
2024-03-05 11:50:21 -0500

    adding new CSV

--- data_analysis.py
+++ data_analysis.py
@@ -1,13 +1,15 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+
 
 # Import the first data set
-data_set1 = pd.read_csv('Monopoly - Sheet1.csv')
+data_set1 = pd.read_csv('Monopoly_Sheet1.csv')
 
 # Import the second data set
-data_set2 = pd.read_csv('Monopoly - Sheet3.csv')
+data_set2 = pd.read_csv('Monopoly_Sheet3.csv')
 
 # Import the third data set
-data_set3 = pd.read_csv('Monopoly Simulator Counts.csv')
+data_set3 = pd.read_csv('Monopoly_Simulator_Counts.csv')
 
 # Combine the three data sets into one data frame
 data_frame = pd.concat([data_set1, data_set2, data_set3])
@@ -16,7 +18,39 @@ data_frame = pd.concat([data_set1, data_set2, data_set3])
 print(data_frame)
 
 # Save the combined data frame to a new CSV file
-data_frame.to_csv('new_monoploy_data.csv', index=False)
+data_frame.to_csv('new_data.csv', index=True)
 
 # Print the first 5 rows of the combined data frame
 print(data_frame.head())
+
+# Print the last 5 rows of the combined data frame
+print(data_frame.tail())
+
+# Print the summary statistics of the combined data frame
+print(data_frame.describe())
+
+# Print the shape of the combined data frame
+print(data_frame.shape)
+
+# Print the column names of the combined data frame
+print(data_frame.columns)
+
+# Print the index of the combined data frame
+print(data_frame.index)
+
+# Print the values of the combined data frame
+print(data_frame.values)
+
+# Print the index of the combined data frame
+print(data_frame.index)
+
+# Do some data visualization with seaborn
+import seaborn as sns
+
+# Do some data visualization with seaborn
+sns.pairplot(data_frame)
+
+# Show the plot
+plt.show()
+
+
