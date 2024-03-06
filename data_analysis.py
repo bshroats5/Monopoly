ad019d2ced49db27f3ea7ed4e110d146864b1365
Bret Shroats <bshroats5@gmail.com>
2024-03-05 21:00:26 -0500

    Update data_analysis.py

--- data_analysis.py
+++ data_analysis.py
@@ -4,39 +4,36 @@ import seaborn as sns
 
 
 # Import the first data set
-data_set1 = pd.read_csv('Monopoly_Sheet1.csv')
+data_set1 = pd.read_csv('Monopoly - Sheet3.csv')
 
 # Import the second data set
-data_set2 = pd.read_csv('Monopoly_Sheet3.csv')
+data_set2 = pd.read_csv('Monopoly_Simulator_Counts.csv')
 
-# Import the third data set
-data_set3 = pd.read_csv('Monopoly_Simulator_Counts.csv')
-
-# Combine the three data sets into one data frame
-data_frame = pd.concat([data_set1, data_set2, data_set3])
+# Combine the two data sets into one data frame
+df = pd.concat([data_set1, data_set2])
 
 # Print the combined data frame
-print(data_frame)
+print(df)
 
 # Save the combined data frame to a new CSV file
-data_frame.to_csv('new_data.csv', index=False)
+df.to_csv('new_data.csv', index=False)
 
 # creating the data visualizations
 # Most landed on spaces
-most_landed = data_frame.nlargest(10, 'Count')
-sns.barplot(x='Count', y='Space Name', data=most_landed)
+most_landed = df.nlargest(10, 'Count of Space Name')
+sns.scatterplot(x='Space Name', y='Count of Space Name', data=most_landed)
 plt.title('Top 10 Most Landed On Spaces')
 plt.show()
 
 # Least landed on spaces
-least_landed = data_frame.nsmallest(10, 'Count')
-sns.barplot(x='Count', y='Space Name', data=least_landed)
+least_landed = df.nsmallest(10, 'Count of Space Name')
+sns.barplot(x='Count of Space Name', y='Space Name', data=least_landed)
 plt.title('Top 10 Least Landed On Spaces')
 plt.show()
 
 # Spaces ranked from most landed on to the least
-sorted_spaces = data_frame.sort_values('Count', ascending=False)
-sns.barplot(x='Count', y='Space Name', data=sorted_spaces)
+sorted_spaces = df.sort_values('Count of Space Name', ascending=False)
+sns.barplot(x='Count of Space Name', y='Space Name', data=sorted_spaces)
 plt.title('Spaces Ranked From Most Landed On to Least')
 plt.show()
 
